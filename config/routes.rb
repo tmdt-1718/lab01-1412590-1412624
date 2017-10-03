029ecea4ab222ab1a7aaa570b9b8d26f97826685
Trung <trungpq999@gmail.com>
2017-10-04 00:31:50 +0700

    add controller for navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
   root 'welcome#index'
-  get '/articles', to: 'articles#index'
+  get '/about', to: 'about#index'
+  get '/albums', to: 'albums#index'
+  get '/blogs', to: 'blogs#index'
+  get '/home', to: 'welcome#index'
 end
